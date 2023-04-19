5a093c2f7c4c094f9a3f1a3535b8818daa97da85
litao <li.tao@klotz.de>
2023-04-19 14:39:11 +0800

    init springboot project

---
+++ Salmpe01/src/main/java/org/example/controller/MyController.java
@@ -0,0 +1,14 @@
+package org.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class MyController {
+    @GetMapping
+    public String Home(){
+        return  "Hello World!";
+    }
+}
